c4cca6291d3012a33cc2b37e09d50a83e472fa04
sytix1@hotmail.com <86479168+Lyudmil-Giant@users.noreply.github.com>
2023-09-09 23:17:38 +0200

    enhanced input system setup in the project

---
+++ Source/ToonTanks/Tank.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tank.h"
+
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Components/InputComponent.h"
+
+ATank::ATank()
+{
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("Spring Arm");
+	SpringArm->SetupAttachment(RootComponent);
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Camera->SetupAttachment(SpringArm);
+}
+
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	if (PlayerController)
+	{
+		UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+		if (Subsystem)
+		{
+			Subsystem->AddMappingContext(PlayerMappingContext, 0);
+		}
+	}
+}
+
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+	if(EnhancedInputComponent)
+	{
+		EnhancedInputComponent->BindAction(MoveAction,ETriggerEvent::Triggered,this, &ATank::Move);
+	}
+}
+
+void ATank::Move(const FInputActionValue& Value)
+{
+	float AxisValue = Value.Get<float>();
+	FString valueString = FString::SanitizeFloat(AxisValue);
+	UE_LOG(LogTemp, Log, TEXT("axisValue : %s"), *valueString);
+}
